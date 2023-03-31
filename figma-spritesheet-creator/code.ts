cdf6313ac2d3c178c9b39c8c666dffc409d33c1a
riya09 <faria.ayir@gmail.com>
2023-04-01 00:39:41 +0600

    feat: append grouped element to new page

--- figma-spritesheet-creator/code.ts
+++ figma-spritesheet-creator/code.ts
@@ -14,19 +14,27 @@ figma.ui.onmessage = async (msg) => {
       // Group the selected elements
       const group = figma.group(selection, figma.currentPage);
       figma.currentPage.selection = [group];
-      // position the group at the start
-      group.x = 0
-      group.y = 0
+
+       // Set the x and y position of the group to the top left of the viewport
+      group.x = 0;
+      group.y = 0;
       
       // Place the children of the group side by side without any gap
       const children = group.children;
       const spacing = 0; // Set the spacing between the children to 0
-      let x = group.x; // Start with the x position of the first child
+      let x = 0; // Start with the x position of the first child
       for (const child of children) {
         child.x = x;
-        child.y = group.y
+        child.y = 0;
         x += child.width + spacing;
       }
+
+      // Create a new page and set it as the current page
+      const newPage = figma.createPage();
+      figma.currentPage = newPage;
+      
+      // Move the group to the new page and set its position to the top left of the viewport
+      newPage.appendChild(group);
       
       figma.viewport.scrollAndZoomIntoView([group]);
     }
