659fad2db3a77334a8e5a640aed5208512eeaf8c
riya09 <faria.ayir@gmail.com>
2023-03-31 23:27:12 +0600

    feat: basic logic for resizing and grouping

--- figma-spritesheet-creator/code.ts
+++ figma-spritesheet-creator/code.ts
@@ -1,8 +1,34 @@
-figma.showUI(__html__, { width: 240, height: 120 });
+figma.showUI(__html__, { width: 240, height: 160 });
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === 'countSelected') {
-    const count = figma.currentPage.selection.length;
-    figma.ui.postMessage({ type: 'selectedCount', count });
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === 'resizeAndGroupSelected') {
+    const width = parseInt(msg.width);
+    const height = parseInt(msg.height);
+    const selection = figma.currentPage.selection;
+    if (selection.length > 1) {
+    // Resize each selected element to the specified width and height
+      for (const node of selection) {
+        node.resize(width, height);
+      }
+      
+      // Group the selected elements
+      const group = figma.group(selection, figma.currentPage);
+      figma.currentPage.selection = [group];
+      // position the group at the start
+      group.x = 0
+      group.y = 0
+      
+      // Place the children of the group side by side without any gap
+      const children = group.children;
+      const spacing = 0; // Set the spacing between the children to 0
+      let x = group.x; // Start with the x position of the first child
+      for (const child of children) {
+        child.x = x;
+        child.y = group.y
+        x += child.width + spacing;
+      }
+      
+      figma.viewport.scrollAndZoomIntoView([group]);
+    }
   }
 };
