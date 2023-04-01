14ff4ac7bb848b1dbdb3fa74468671f233681bc6
riya09 <faria.ayir@gmail.com>
2023-04-01 13:33:00 +0600

    feat: logic and ux flow update

--- figma-spritesheet-creator/code.ts
+++ figma-spritesheet-creator/code.ts
@@ -4,20 +4,28 @@ figma.ui.onmessage = async (msg) => {
   if (msg.type === 'resizeAndGroupSelected') {
     const width = parseInt(msg.width);
     const height = parseInt(msg.height);
+    // Get all selected nodes and store them in an array
     const selection = figma.currentPage.selection;
     if (selection.length > 1) {
-    // Resize each selected element to the specified width and height
+      // Group the selected elements
+      // Create a new page and set it as the current page
+      const newPage = figma.createPage();
+      figma.currentPage = newPage;
+
+      const cloneNodes = []
       for (const node of selection) {
+        const cloneNode = node.clone()
+        cloneNodes.push(cloneNode)
+        newPage.appendChild(cloneNode);
+      }
+
+      const group = figma.group(cloneNodes, newPage);
+      newPage.selection = [group];
+
+      // Resize each selected element to the specified width and height
+      for (const node of group.children) {
         node.resize(width, height);
       }
-      
-      // Group the selected elements
-      const group = figma.group(selection, figma.currentPage);
-      figma.currentPage.selection = [group];
-
-       // Set the x and y position of the group to the top left of the viewport
-      group.x = 0;
-      group.y = 0;
       
       // Place the children of the group side by side without any gap
       const children = group.children;
@@ -28,13 +36,6 @@ figma.ui.onmessage = async (msg) => {
         child.y = 0;
         x += child.width + spacing;
       }
-
-      // Create a new page and set it as the current page
-      const newPage = figma.createPage();
-      figma.currentPage = newPage;
-      
-      // Move the group to the new page and set its position to the top left of the viewport
-      newPage.appendChild(group);
       
       figma.viewport.scrollAndZoomIntoView([group]);
     }
